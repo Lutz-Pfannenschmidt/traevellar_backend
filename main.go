dc5b63c6a6f924d2e1efe3d763e2355395b421b3
Lutz Pfannenschmidt <lutz@lutz-pfannenschmidt.de>
2024-09-12 15:27:36 +0200

    some testing code

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ var db api.Api
 func main() {
 	db = *api.NewApi(API_URL)
 
-	locations, err := db.GetLocations("Holtwick", 99, true, true, true, "en")
+	locations, err := db.GetLocations("Holtwick", 10, true, true, true, "en")
 	if err != nil {
 		panic(err)
 	}
@@ -35,21 +35,19 @@ func main() {
 	yagll.Infof("Location by id: %s %s", byId.ID, byId.Name)
 
 	unixTime := time.Now().Unix()
-	_, err = db.GetDepartures("906075", strconv.FormatInt(unixTime, 10), 100, 99, true, true, api.Products{
-		NationalExpress: true,
-		National:        true,
-		RegionalExpress: true,
-		Regional:        true,
-		Suburban:        true,
-		Bus:             true,
-		Ferry:           true,
-		Subway:          true,
-		Tram:            true,
-		Taxi:            true,
-	})
+	departures, err := db.GetDepartures("906075", strconv.FormatInt(unixTime, 10), 100, 99, true, true, *api.TRAIN_PRODUCTS)
 
 	if err != nil {
 		panic(err)
+	}
+
+	for _, departure := range departures {
+		yagll.Infof("Departure: %s %s %s %s", departure.TripId, departure.When, departure.Platform, departure.Direction)
+		yagll.Infof("Stop: %s %s", departure.Stop.ID, departure.Stop.Name)
+		yagll.Infof("Line: %s %s %s %s", departure.Line.ID, departure.Line.Name, departure.Line.Product, departure.Line.ProductName)
+		for _, remark := range departure.Remarks {
+			yagll.Infof("Remark: %v", remark)
+		}
 	}
 
 }
